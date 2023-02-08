92d5847ed4fc262d27d388f04caff3edcaf65af1
jdallagnol <joao.dallagnol1@gmail.com>
2023-02-08 11:04:53 -0300

    Initial Commit

---
+++ UploadingFilesExercise/UploadingFilesExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UploadingFilesExerciseApp.swift
+//  UploadingFilesExercise
+//
+//  Created by João Dall Agnol on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct UploadingFilesExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
